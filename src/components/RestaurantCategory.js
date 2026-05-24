da5116e938f6861d0a7632756367dee3448f3a48
jubilee5 <jubileesharma17@gmail.com>
2026-05-24 16:12:58 +0530

    implemented controlled component

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
+const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
 
-    const [showItems, setShowItems] = useState(false);
+    // const [showItems, setShowItems] = useState(false);
 
     const handleClick = () => {
-        setShowItems(!showItems);
+        // setShowItems(!showItems);
+        setShowIndex(); // we are calling the setShowIndex function which is passed as a prop from the parent component. This function will update the showIndex state variable in the parent component with the index of the clicked category. This way we can manage the show/hide state of all the categories in one place which is the restaurant menu component.
     };
+
+    // if the restaurant category had its own state variable to show or hide the items, then we would have to maintain a separate state variable for each restaurant category. This would make our code more complex and harder to maintain. Instead, we can lift the state up to the parent component and pass it down as props to the child component. This way, we can manage the state in one place and pass it down to all the child components that need it. This is a common pattern in React applications called "lifting state up". having its own state makes it a uncontrolled component. By lifting the state up to the parent component, we can make it a controlled component.
 
     return (
         <div>
@@ -36,3 +39,7 @@ const RestaurantCategory = ({ data }) => {
 };
 
 export default RestaurantCategory;
+
+
+
+// now the restaurant category component is a controlled component. The parent component will manage the state of showItems and pass it down as props to the restaurant category component. 
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
@@ -6,7 +7,9 @@ import RestaurantCategory from "./RestaurantCategory";
 const RestaurantMenu = () => {
     const {resId} = useParams(); // this is a dummy restaurant id. We will get the actual restaurant id from the url when we click on a restaurant card. We can use useParams hook from react-router-dom to get the restaurant id from the url.
     const resInfo = useRestaurantMenu(resId); // this is a custom hook that we will create to fetch the restaurant menu data. We will pass the restaurant id as an argument to the custom hook and it will return the restaurant menu data. We can use this restaurant menu data to show the restaurant menu in the UI.
- 
+
+    const [showIndex, setShowIndex] = useState(null); // this state variable is used to show or hide the items of a restaurant category. We will pass this state variable as a prop to the restaurant category component and it will use this state variable to show or hide the items of that category. We will also pass a function to update this state variable to the restaurant category component so that it can update the state variable when the user clicks on the category title. This way we can manage the show/hide state of all the categories in one place which is the restaurant menu component.
+
 if (resInfo === null) {
     return <Shimmer />;
 }
@@ -38,10 +41,12 @@ const categories =
             </p>
 
             <div>
-                {categories?.map((category) => (
+                {categories?.map((category, index) => (
                     <RestaurantCategory
                         key={category?.card?.card?.title}
                         data={category?.card?.card}
+                        showItems={index===showIndex ? true :false} 
+                        setShowIndex={() => setShowIndex(index)} // we are passing a function to update the showIndex state variable. When the user clicks on the category title, this function will be called and it will update the showIndex state variable with the index of the clicked category. This way we can show or hide the items of the clicked category based on the value of showIndex state variable.
                     />
                 ))}
             </div>
