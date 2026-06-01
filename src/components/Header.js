97abc5c44cb9e52c71d7eacfad0a59ae2c659250
jubilee5 <jubileesharma17@gmail.com>
2026-06-01 14:44:32 +0530

    modified header ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,36 +27,36 @@ const Header = () => {
    
 
     return (
-        <div className="flex justify-between bg-pink-50 shadow-lg ">
+        <div className="flex justify-between bg-linear-to-r from-pink-100 to-pink-50 shadow-xl px-4">
             <div className="logo-container">
-                <img className="w-20 p-4 justify-content m-2" src={LOGO_URL} alt="logo"/>        
+                <img className="w-20  p-4 transition-transform duration-300 hover:scale-110" src={LOGO_URL} alt="logo"/>        
             </div>
             <div className="flex items-center">
-                <ul className="flex p-4 m-4">
-                    <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
-                    <li className="px-4">
+                <ul className="flex items-center gap-6 text-gray-700 font-medium">
+                    <li className="">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+                    <li className="px-4 hover:text-pink-600 transition duration-200 cursor-pointer">
                         <Link to="/">Home</Link>
                     </li>
-                    <li className="px-4">
+                    <li className="px-4 hover:text-pink-600 transition duration-200 cursor-pointer">
                         <Link to="/about">About Us</Link>
                     </li>
-                    <li className="px-4">
+                    <li className="px-4 hover:text-pink-600 transition duration-200 cursor-pointer">
                         <Link to="/contact">Contact Us</Link>
                     </li>
-                    <li className="px-4">
+                    <li className="px-4 hover:text-pink-600 transition duration-200 cursor-pointer">
                         <Link to="/grocery">Grocery</Link>
                     </li>
-                    <li className="px-4 font-bold">
-                        <Link to="/cart">Cart - ({cartItems.length} items)</Link>
+                    <li className="px-4 hover:text-pink-600 transition duration-200 cursor-pointer">
+                        <Link to="/cart">  🛒 Cart ({cartItems.length} items)</Link>
                     </li>
-                    <button className="login" onClick={() => {
+                    <button className="bg-pink-500 text-white px-5 py-2 rounded-full font-semibold shadow-md hover:bg-pink-600 hover:scale-105 transition duration-200" onClick={() => {
                         btnName === "Login" 
                         ? setBtnName("Logout") 
                         : setBtnName("Login");
                     }}>
                         {btnName}
                     </button>
-                     <li className="px-4 font-bold">{loggedInUser}</li>
+                     <li className="font-bold text-pink-700 px-4 py-2 ">{loggedInUser}</li>
                    
                 </ul>
             </div>
